710cf63721d5927c98ed31c6143b454ff1f98a29
CarlosArancibia <arancibiacarlos.b@gmail.com>
2024-02-24 18:25:53 -0400

    readme updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl">Hello World</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-5xl mb-6">Hello World</h1>
+      <Link href={'/about'}>About Page</Link>
     </main>
   );
 }
